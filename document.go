834f9b8978a62401afabcd9c61a2a64b14e1189b
liuyixuan <754689243@qq.com>
2024-09-09 12:00:49 +0800

    添加 pdf 相关操作

--- document.go
+++ document.go
@@ -6,28 +6,27 @@ import (
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/types"
 )
 
-// 检验 PDF 文件
-func PDFValidateFile(files []string) error {
-	if _, error := cli.Process(cli.ValidateCommand(files, nil)); error != nil {
+// 执行 cli 命令
+func iCliExec(command *cli.Command) error {
+	if _, error := cli.Process(command); error != nil {
 		return error
 	}
 	return nil
+}
+
+// 检验 PDF 文件
+func PDFValidateFile(files []string) error {
+	return iCliExec(cli.ValidateCommand(files, nil))
 }
 
 // 创建 pdf 文件
 func PDFCreateFile(inFilePDF, inFileJSON, outFilePDF string) error {
-	if _, error := cli.Process(cli.CreateCommand(inFilePDF, inFileJSON, outFilePDF, nil)); error != nil {
-		return error
-	}
-	return nil
+	return iCliExec(cli.CreateCommand(inFilePDF, inFileJSON, outFilePDF, nil))
 }
 
 // 合并 pdf 文件
 func PDFMergeFiles(inFilesPDF []string, outFilePDF string) error {
-	if _, error := cli.Process(cli.MergeCreateCommand(inFilesPDF, outFilePDF, true, nil)); error != nil {
-		return error
-	}
-	return nil
+	return iCliExec(cli.MergeCreateCommand(inFilesPDF, outFilePDF, true, nil))
 }
 
 // 图片转 pdf 文件
@@ -37,19 +36,10 @@ func PDFImportImages(inFilesIMG []string, outFilePDF, description string) error 
 	if error != nil {
 		return error
 	}
-
-	if _, error := cli.Process(cli.ImportImagesCommand(inFilesIMG, outFilePDF, imgImport, nil)); error != nil {
-		return error
-	}
-
-	return nil
+	return iCliExec(cli.ImportImagesCommand(inFilesIMG, outFilePDF, imgImport, nil))
 }
 
 // pdf 导出图片
 func PDFExportImages(inFile, outDir string, pageSelection []string) error {
-	if _, error := cli.Process(cli.ExtractImagesCommand(inFile, outDir, pageSelection, nil)); error != nil {
-		return error
-	}
-
-	return nil
+	return iCliExec(cli.ExtractImagesCommand(inFile, outDir, pageSelection, nil))
 }
